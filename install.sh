a73affd288b7b410ca9ea05728613002492e7c2b
alvaro-cas <alvaro_cas@protonmail.com>
2023-01-10 22:11:35 -0600

    Add --noconfirm

--- install.sh
+++ install.sh
@@ -1,16 +1,17 @@
 echo -e "Hello, $(whoami)!"
 # Ask to continue
-echo -n "Install configuration files? (doing this will erase any previous configuration)"
+echo -e "Install configuration files?"
+echo -n "(doing this will erase any previous configuration) [y/N] "
 
 read "answer"
 
 if [ "$answer" == "Y" -o "$answer" == "y" ]
 then
   # Install pacman applications
-  sudo pacman -S neofetch pulseaudio-alsa alsa-utils playerctl zip unzip signal-desktop kitty firefox rofi xclip firefox-developer-edition nemo veracrypt keepassxc ueberzug graphicsmagick ghostscript obsidian ntfs-3g maim peek gifski zbar iw noto-fonts-emoji noto-fonts-cjk rofi-emoji xdotool emacs neovim
+  sudo pacman --noconfirm -S neofetch pulseaudio-alsa alsa-utils playerctl zip unzip signal-desktop kitty firefox rofi xclip firefox-developer-edition nemo veracrypt keepassxc ueberzug graphicsmagick ghostscript obsidian ntfs-3g maim peek gifski zbar iw noto-fonts-emoji noto-fonts-cjk rofi-emoji xdotool emacs neovim
 
   # Install yay applications
-  yay -S onlyoffice-bin lf picom-jonaburg-git spacefm nvm spotify
+  yay --noconfirm -S onlyoffice-bin lf picom-jonaburg-git spacefm nvm spotify
 
   # Copy configuration to ~/.config folder
   sudo cp ./TTF/* /usr/share/fonts
