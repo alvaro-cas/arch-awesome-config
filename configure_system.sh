c09b1d64f3606eaef6a52087da88f975401a4b11
Álvaro <cuatroy2.com>
2023-01-22 19:14:16 -0600

    Add poppler

--- configure_system.sh
+++ configure_system.sh
@@ -20,7 +20,7 @@ then
     cd ../ && sudo rm -r yay
 
     # Install pacman applications
-    sudo pacman -S --noconfirm neofetch pulseaudio-alsa alsa-utils playerctl zip unzip signal-desktop kitty firefox rofi xclip firefox-developer-edition nemo veracrypt keepassxc ueberzug graphicsmagick ghostscript obsidian ntfs-3g maim peek gifski zbar iw noto-fonts-emoji noto-fonts-cjk rofi-emoji xdotool emacs neovim vicious
+    sudo pacman -S --noconfirm neofetch pulseaudio-alsa alsa-utils playerctl zip unzip signal-desktop kitty firefox rofi xclip firefox-developer-edition nemo veracrypt keepassxc ueberzug graphicsmagick ghostscript obsidian ntfs-3g maim peek gifski zbar iw noto-fonts-emoji noto-fonts-cjk rofi-emoji xdotool emacs neovim vicious poppler
 
     # Install yay applications
     yay -S --noconfirm onlyoffice-bin lf picom-jonaburg-git spacefm nvm spotify
